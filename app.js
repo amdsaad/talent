a7cc1a90349280ee50dedcfd672a96ead4ee5a44
Ahmed Saad <saadamd@gmail.com>
2018-07-15 03:27:25 +0400

    handled auth

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//passport config
+require('./config/passport')(passport);
+
+//load routes
+const auth = require('./routes/auth');
 
 const app = express();
-
-const port = process.env.PORT || 5000;
-
 
 app.get('/',(req, res)=>{
   res.send('It Works');
 });
+
+//Use routes
+app.use('/auth',auth);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
   console.log(`Server started on port ${port}`)
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports ={
+  mongoURI:'mongodb://amdsaad:Shaw6363848$$@ds239071.mlab.com:39071/storybooks',
+  googleClientID:'601301842281-1qpc1gum3hs37vmmhrbjm53mclplrgrl.apps.googleusercontent.com',
+  googleClientSecret:'p-9ErnPZD7dbhkwKBKrkdBl6'
+}
