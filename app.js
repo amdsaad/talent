9fb8397f475577bbb1957e0f1d5b9f4f397b2159
Ahmed Saad <saadamd@gmail.com>
2018-07-16 18:55:22 +0400

    version 1.0.0alpha done

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 
 
-//load user model
+//load models
 require('./models/User');
+require('./models/Story');
 
 //passport config
 require('./config/passport')(passport);
@@ -15,50 +19,78 @@ require('./config/passport')(passport);
 //load routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
+const stories = require('./routes/stories');
 
 //load keys
 const keys = require('./config/keys');
+
+//Handlebars Helpers
+const {
+  truncate,
+  stripTags,
+  formatDate,
+  select,
+  editIcon
+} = require('./helpers/hbs');
 
 //map global promise
 mongoose.Promise = global.Promise;
 
 //mongoose connect
-mongoose.connect(keys.mongoURI,{
+mongoose.connect(keys.mongoURI, {
   useNewUrlParser: true
 })
-  .then(()=> console.log('MongoDB Connected'))
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
 const app = express();
 
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Method verride Middelware
+app.use(methodOverride('_method'));
+
 //Handlebars Middleware
-app.engine('handlebars',exphbs({
-defaultLayout: 'main'
+app.engine('handlebars', exphbs({
+  helpers: {
+    truncate: truncate,
+    stripTags: stripTags,
+    formatDate,
+    select,
+    editIcon
+  },
+  defaultLayout: 'main'
 }));
-app.set('view engine','handlebars');
+app.set('view engine', 'handlebars');
 
 app.use(cookieParser());
 app.use(session({
-  secret:"secret",
-  resave:false,
-  saveUninitialized:false
+  secret: "secret",
+  resave: false,
+  saveUninitialized: false
 }))
 //passport midleware
 app.use(passport.initialize());
 app.use(passport.session());
 
 //set Global vars
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
   res.locals.user = req.user || null;
   next();
 });
 
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 //Use routes
-app.use('/',index);
-app.use('/auth',auth);
+app.use('/', index);
+app.use('/auth', auth);
+app.use('/stories', stories);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`Server started on port ${port}`)
 });
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports ={
-  mongoURI:'mongodb://amdsaad:Shaw6363848$$@ds239071.mlab.com:39071/storybooks',
-  googleClientID:'601301842281-1qpc1gum3hs37vmmhrbjm53mclplrgrl.apps.googleusercontent.com',
-  googleClientSecret:'p-9ErnPZD7dbhkwKBKrkdBl6'
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./keys_prod')
+}else{
+  module.exports = require('./keys_dev')
 }
