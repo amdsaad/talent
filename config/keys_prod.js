210616aec7fe77542644ff118111e3c3fbaee694
Ahmed Saad <saadamd@gmail.com>
2018-07-19 03:26:48 +0400

    facebook oauth-working local

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -5,7 +5,8 @@ module.exports = {
 
   facebook: {
     clientID: process.env.FACEBOOK_CLIENT_ID,
-    clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    callbackURL:process.env.FACEBOOK_CALLBACK_URL
   }
 
 }
