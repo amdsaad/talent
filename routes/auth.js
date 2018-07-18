6e3e2ab99aea69978bba4285565c1855805394e3
Ahmed Saad <saadamd@gmail.com>
2018-07-18 16:25:08 +0400

    FB login2

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+router.get('/facebook', passport.authenticate('facebook'));
 
 router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
