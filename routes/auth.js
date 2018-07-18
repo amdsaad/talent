f4d9502e9decfd6b37c2d335de1690ba08422115
Ahmed Saad <saadamd@gmail.com>
2018-07-18 23:35:56 +0400

    facebook oauth

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.get('/facebook', passport.authenticate('facebook'));
+router.get('/facebook',
+  passport.authenticate('facebook'));
 
 router.get('/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/' }),
-  (req, res) => {
-    res.redirect('/dashboard');
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
   });
+
 
 
 router.get('/verify', (req, res)=>{
