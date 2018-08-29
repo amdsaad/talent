a54fa8fe2bc919f96bb2805cb263fc2903152448
Ahmed Saad <saadamd@gmail.com>
2018-08-29 19:27:59 +0400

    “googleID”

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,7 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
-  //googleClientID: process.env.GOOGLE_CLIENT_ID,
-  //googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 
   facebook: {
     clientID: process.env.FACEBOOK_CLIENT_ID,
