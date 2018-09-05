16e228eb5d0f95693cbf810fc7793225b50faf52
Ahmed Saad <saadamd@gmail.com>
2018-09-05 22:10:11 +0400

    cloundary ids

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -7,6 +7,12 @@ module.exports = {
     clientID: process.env.FACEBOOK_CLIENT_ID,
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     callbackURL:process.env.FACEBOOK_CALLBACK_URL
+  },
+  cloudinary:{
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
   }
+
 
 }
